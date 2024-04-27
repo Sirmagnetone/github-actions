d7d3fe70920bff69d81e3001fb7985b5481bd93f
Sirmagnetone <javierrg11.jrg@gmail.com>
2024-04-27 16:18:16 +0200

    Change

--- src/main/java/es/codeurjc/test/ejem/Calculadora.java
+++ src/main/java/es/codeurjc/test/ejem/Calculadora.java
@@ -3,6 +3,7 @@ package es.codeurjc.test.ejem;
 public class Calculadora {
 	// Change
 	//blafeioeajfeajfo
+	//wgjoiagr
 	//This shofeojsjrsg
 	public double suma(double op1, double op2) {
 		return op1 + op2;
